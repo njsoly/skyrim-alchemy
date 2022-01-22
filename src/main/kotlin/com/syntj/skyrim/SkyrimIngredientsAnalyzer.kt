d8328008f07eedbcc4970720c05c3cba87c82978
njsoly <njsoly@syntj.com>
2022-01-22 11:31:27 -0600

    SkyrdimIngredientsAnalyzer: change println to logger.info

--- src/main/kotlin/com/syntj/skyrim/SkyrimIngredientsAnalyzer.kt
+++ src/main/kotlin/com/syntj/skyrim/SkyrimIngredientsAnalyzer.kt
@@ -1,11 +1,16 @@
 package com.syntj.skyrim
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 class SkyrimIngredientsAnalyzer {
 
-//    val ingredientList: IngredientList
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(SkyrimIngredientsAnalyzer::class.java)
+    }
 
     fun run() {
-        println("hi there, welcome to ")
+        println("hi there, welcome to ${this.javaClass.simpleName}")
 
         val ingredients = IngredientsJsonImporter().readIngredientsJson()
 
@@ -13,17 +18,17 @@ class SkyrimIngredientsAnalyzer {
         val heaviest = ingredients.maxByOrNull { it.weight }!!
         val zeroWeight = ingredients.filter { ingredient -> ingredient.weight == 0.0 }
         val zeroValue = ingredients.filter { ingredient -> ingredient.value == 0.0 }
-        println("ingredients has ${ingredients.size} in it.")
-        println("most expensive is ${mostExpensive?.name}, at ${mostExpensive.value}.")
-        println("heaviest is ${heaviest?.name}, at ${heaviest.weight}.")
+        logger.info("ingredients has ${ingredients.size} in it.")
+        logger.info("most expensive is ${mostExpensive?.name}, at ${mostExpensive.value}.")
+        logger.info("heaviest is ${heaviest?.name}, at ${heaviest.weight}.")
 
         if (zeroWeight.isNotEmpty()) {
-            println(
+            logger.info(
                 "${zeroWeight.size} ingredients have no weight: \n" +
                         zeroWeight.map { it.name }.joinToString(separator = ",\n")
             )
         } else {
-            println(
+            logger.info(
                 "${zeroWeight.size} ingredients have no weight.")
 
         }
