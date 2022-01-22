1f61645d2a1dd2013958a98abf07aee2de471b65
njsoly <njsoly@syntj.com>
2022-01-22 10:58:32 -0600

    SkyrimIngredientsAnalyzer: remove little class from main file

--- src/main/kotlin/SkyrimIngredientsAnalyzer.kt
+++ src/main/kotlin/SkyrimIngredientsAnalyzer.kt
@@ -1,25 +1,15 @@
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 
-class ReadIngredientsJson {
-
-    companion object {
-        const val JSON_PATH = "data/ingredients.json"
-        val objectMapper = ObjectMapper().registerModule(KotlinModule())
-    }
-
-}
 
 fun main() {
     println("hi there")
 
 
     val ingredients = IngredientsJsonImporter().readIngredientsJson()
-//    val ingredients = objectMapper.readValue(File("data/ingredients.json"), IngredientList::class.java).ingredients
-
 
     val mostExpensive = ingredients.maxByOrNull{ it.value }
     val heaviest = ingredients.maxByOrNull { it.weight }
+    val zeroWeight = ingredients.filter{ ingredient -> ingredient.weight == 0.0 }
+    val zeroValue = ingredients.filter{ ingredient -> ingredient.value == 0.0 }
     println("ingredients has ${ingredients.size} in it.")
     println("most expensive is ${mostExpensive?.name}, at ${mostExpensive?.value}.")
     println("heaviest is ${heaviest?.name}, at ${heaviest!!.weight}.")
