647deebaa7b097fc7229dabdf0488fe97f3c61ea
njsoly <njsoly@syntj.com>
2022-01-23 02:16:00 -0600

    MISC-24: Some I/O error handling

--- src/main/kotlin/com/syntj/skyrim/IngredientsJsonImporter.kt
+++ src/main/kotlin/com/syntj/skyrim/IngredientsJsonImporter.kt
@@ -6,6 +6,8 @@ import com.syntj.skyrim.SkyrimAlchemyConstants.JSON_PATH
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
 
 class IngredientsJsonImporter {
 
@@ -16,6 +18,23 @@ class IngredientsJsonImporter {
 
     fun readIngredientsJson(path: String = JSON_PATH): List<Ingredient> {
 
+        checkIfFileIsReadable(path)
+
         return objectMapper.readValue(File(path), IngredientList::class.java).ingredients
+    }
+
+    private fun checkIfFileIsReadable(path: String) {
+        when {
+            !Files.exists(Path.of(path)) -> {
+                throw Exception("Path $path doesn't exist")
+            }
+            !Files.isRegularFile(Path.of(path)) -> {
+                throw Exception("Path $path is not a regular file.")
+            }
+            !Files.isReadable(Path.of(path)) -> {
+                throw Exception("Path $path is not readable.")
+            }
+        }
+
     }
 }
